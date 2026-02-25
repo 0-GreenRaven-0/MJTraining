cad54209b7628070a0670e88a482f6abcb0cf565
Matthew Hamdesh <greenraven1000@gmail.com>
2026-02-25 23:33:07 +0200

    Version 6.7

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-/* import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -17,4 +17,3 @@ createRoot(document.getElementById('root')).render(
     </HeadProvider>
   </StrictMode>,
 )
- */
